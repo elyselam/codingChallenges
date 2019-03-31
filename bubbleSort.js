1b0f022f6453aeb83fdd8eae8f7380db9ee81b6b
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2019-03-30 20:11:04 -0700

    .

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,26 @@
-function bubbleSort(arr)
 
-console.log(bubbleSort([2,1,0,-1,0,2,-7]));
+function swap(arr, i1, i2) {
+	var temp = arr[i1];
+	arr[i1] = arr[i2];
+	arr[i2] = temp;
+}
+
+function bubblesort(arr) {
+	var swapped = true;
+	// keep going unless no elements can be swapped anymore
+	while (swapped) {	
+		// set swapped to false so that the loop stops unless two element are actually swapped
+		swapped = false; //false if no swapping is done
+
+		// loop through the whole array swapping adjacent elements
+		for (var i = 1; i < arr.length; i++) {
+			if (arr[i-1] > arr[i]) {
+				swap(arr, i-1, i);
+				//do more while loop to check if numbers need to be swapped again
+				swapped = true;
+			}
+		}	
+	}
+	return arr;
+}
+bubblesort([103, 45, 2, 1, 97, -4, 67]); 
